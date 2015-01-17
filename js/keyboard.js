8d5dc01dad043ca44fce9af51af0df430e89c891
gbytz <gbytz.zyt@gmail.com>
2015-01-16 21:30:32 -0300

    Renaming constants in KeyboardHandler module

--- js/keyboard.js
+++ js/keyboard.js
@@ -7,166 +7,166 @@
 
 // Static properties:
     // Arrows
-    KeyboardHandler.KEYCODE_LEFT = 'LEFT';
-    KeyboardHandler.KEYCODE_UP = 'UP';
-    KeyboardHandler.KEYCODE_RIGHT = 'RIGHT';
-    KeyboardHandler.KEYCODE_DOWN = 'DOWN';
+    KeyboardHandler.KEY_LEFT = 'LEFT';
+    KeyboardHandler.KEY_UP = 'UP';
+    KeyboardHandler.KEY_RIGHT = 'RIGHT';
+    KeyboardHandler.KEY_DOWN = 'DOWN';
 
     // Digits
-    KeyboardHandler.KEYCODE_0 = '0';
-    KeyboardHandler.KEYCODE_1 = '1';
-    KeyboardHandler.KEYCODE_2 = '2';
-    KeyboardHandler.KEYCODE_3 = '3';
-    KeyboardHandler.KEYCODE_4 = '4';
-    KeyboardHandler.KEYCODE_5 = '5';
-    KeyboardHandler.KEYCODE_6 = '6';
-    KeyboardHandler.KEYCODE_7 = '7';
-    KeyboardHandler.KEYCODE_8 = '8';
-    KeyboardHandler.KEYCODE_9 = '9';
+    KeyboardHandler.KEY_0 = '0';
+    KeyboardHandler.KEY_1 = '1';
+    KeyboardHandler.KEY_2 = '2';
+    KeyboardHandler.KEY_3 = '3';
+    KeyboardHandler.KEY_4 = '4';
+    KeyboardHandler.KEY_5 = '5';
+    KeyboardHandler.KEY_6 = '6';
+    KeyboardHandler.KEY_7 = '7';
+    KeyboardHandler.KEY_8 = '8';
+    KeyboardHandler.KEY_9 = '9';
 
     // Letters
-    KeyboardHandler.KEYCODE_A = 'A';
-    KeyboardHandler.KEYCODE_B = 'B';
-    KeyboardHandler.KEYCODE_C = 'C';
-    KeyboardHandler.KEYCODE_D = 'D';
-    KeyboardHandler.KEYCODE_E = 'E';
-    KeyboardHandler.KEYCODE_F = 'F';
-    KeyboardHandler.KEYCODE_G = 'G';
-    KeyboardHandler.KEYCODE_H = 'H';
-    KeyboardHandler.KEYCODE_I = 'I';
-    KeyboardHandler.KEYCODE_J = 'J';
-    KeyboardHandler.KEYCODE_K = 'K';
-    KeyboardHandler.KEYCODE_L = 'L';
-    KeyboardHandler.KEYCODE_M = 'M';
-    KeyboardHandler.KEYCODE_N = 'N';
-    KeyboardHandler.KEYCODE_O = 'O';
-    KeyboardHandler.KEYCODE_P = 'P';
-    KeyboardHandler.KEYCODE_Q = 'Q';
-    KeyboardHandler.KEYCODE_R = 'R';
-    KeyboardHandler.KEYCODE_S = 'S';
-    KeyboardHandler.KEYCODE_T = 'T';
-    KeyboardHandler.KEYCODE_U = 'U';
-    KeyboardHandler.KEYCODE_V = 'V';
-    KeyboardHandler.KEYCODE_W = 'W';
-    KeyboardHandler.KEYCODE_X = 'X';
-    KeyboardHandler.KEYCODE_Y = 'Y';
-    KeyboardHandler.KEYCODE_Z = 'Z';
+    KeyboardHandler.KEY_A = 'A';
+    KeyboardHandler.KEY_B = 'B';
+    KeyboardHandler.KEY_C = 'C';
+    KeyboardHandler.KEY_D = 'D';
+    KeyboardHandler.KEY_E = 'E';
+    KeyboardHandler.KEY_F = 'F';
+    KeyboardHandler.KEY_G = 'G';
+    KeyboardHandler.KEY_H = 'H';
+    KeyboardHandler.KEY_I = 'I';
+    KeyboardHandler.KEY_J = 'J';
+    KeyboardHandler.KEY_K = 'K';
+    KeyboardHandler.KEY_L = 'L';
+    KeyboardHandler.KEY_M = 'M';
+    KeyboardHandler.KEY_N = 'N';
+    KeyboardHandler.KEY_O = 'O';
+    KeyboardHandler.KEY_P = 'P';
+    KeyboardHandler.KEY_Q = 'Q';
+    KeyboardHandler.KEY_R = 'R';
+    KeyboardHandler.KEY_S = 'S';
+    KeyboardHandler.KEY_T = 'T';
+    KeyboardHandler.KEY_U = 'U';
+    KeyboardHandler.KEY_V = 'V';
+    KeyboardHandler.KEY_W = 'W';
+    KeyboardHandler.KEY_X = 'X';
+    KeyboardHandler.KEY_Y = 'Y';
+    KeyboardHandler.KEY_Z = 'Z';
 
     // Punctuation
-    KeyboardHandler.KEYCODE_SEMICOLON = 'SEMICOLON';
-    KeyboardHandler.KEYCODE_EQUAL = 'EQUAL';
-    KeyboardHandler.KEYCODE_COMMA = 'COMMA';
-    KeyboardHandler.KEYCODE_MINUS = 'MINUS';
-    KeyboardHandler.KEYCODE_PERIOD = 'PERIOD';
-    KeyboardHandler.KEYCODE_SLASH = 'SLASH';
-    KeyboardHandler.KEYCODE_BACKQUOTE = 'BACKQUOTE';
-    KeyboardHandler.KEYCODE_BRACKETLEFT = 'BRACKETLEFT';
-    KeyboardHandler.KEYCODE_BACKSLASH = 'BACKSLASH';
-    KeyboardHandler.KEYCODE_BRACKETRIGHT = 'BRACKETRIGHT';
-    KeyboardHandler.KEYCODE_QUOTE = 'QUOTE';
+    KeyboardHandler.KEY_SEMICOLON = 'SEMICOLON';
+    KeyboardHandler.KEY_EQUAL = 'EQUAL';
+    KeyboardHandler.KEY_COMMA = 'COMMA';
+    KeyboardHandler.KEY_MINUS = 'MINUS';
+    KeyboardHandler.KEY_PERIOD = 'PERIOD';
+    KeyboardHandler.KEY_SLASH = 'SLASH';
+    KeyboardHandler.KEY_BACKQUOTE = 'BACKQUOTE';
+    KeyboardHandler.KEY_BRACKETLEFT = 'BRACKETLEFT';
+    KeyboardHandler.KEY_BACKSLASH = 'BACKSLASH';
+    KeyboardHandler.KEY_BRACKETRIGHT = 'BRACKETRIGHT';
+    KeyboardHandler.KEY_QUOTE = 'QUOTE';
 
     // No Printable Keys
-    KeyboardHandler.KEYCODE_SHIFTLEFT = 'SHIFTLEFT';
-    KeyboardHandler.KEYCODE_SHIFTRIGHT = 'SHIFTRIGHT';
-    KeyboardHandler.KEYCODE_CTRLLEFT = 'CTRLLEFT';
-    KeyboardHandler.KEYCODE_CTRLRIGHT = 'CTRLRIGHT';
-    KeyboardHandler.KEYCODE_ALTLEFT = 'ALTLEFT';
-    KeyboardHandler.KEYCODE_ALTRIGHT = 'ALTRIGHT';
-    KeyboardHandler.KEYCODE_CAPSLOCK = 'CAPSLOCK';
-    KeyboardHandler.KEYCODE_OSLEFT = 'OSLEFT';
-    KeyboardHandler.KEYCODE_OSRIGHT = 'OSRIGHT';
-    KeyboardHandler.KEYCODE_CONTEXTMENU = 'CONTEXTMENU';
-    KeyboardHandler.KEYCODE_ENTER = 'ENTER';
-    KeyboardHandler.KEYCODE_SPACE = 'SPACE';
-    KeyboardHandler.KEYCODE_TAB = 'TAB';
-    KeyboardHandler.KEYCODE_DELETE = 'DELETE';
-    KeyboardHandler.KEYCODE_END = 'END';
-    KeyboardHandler.KEYCODE_HOME = 'HOME';
-    KeyboardHandler.KEYCODE_INSERT = 'INSERT';
-    KeyboardHandler.KEYCODE_PAGEDOWN = 'PAGEDOWN';
-    KeyboardHandler.KEYCODE_PAGEUP = 'PAGEUP';
-    KeyboardHandler.KEYCODE_ESCAPE = 'ESCAPE';
-    KeyboardHandler.KEYCODE_PRINTSCREEN = 'PRINTSCREEN';
-    KeyboardHandler.KEYCODE_SCROLLLOCK = 'SCROLLLOCK';
-    KeyboardHandler.KEYCODE_PAUSE = 'PAUSE';
+    KeyboardHandler.KEY_SHIFTLEFT = 'SHIFTLEFT';
+    KeyboardHandler.KEY_SHIFTRIGHT = 'SHIFTRIGHT';
+    KeyboardHandler.KEY_CTRLLEFT = 'CTRLLEFT';
+    KeyboardHandler.KEY_CTRLRIGHT = 'CTRLRIGHT';
+    KeyboardHandler.KEY_ALTLEFT = 'ALTLEFT';
+    KeyboardHandler.KEY_ALTRIGHT = 'ALTRIGHT';
+    KeyboardHandler.KEY_CAPSLOCK = 'CAPSLOCK';
+    KeyboardHandler.KEY_OSLEFT = 'OSLEFT';
+    KeyboardHandler.KEY_OSRIGHT = 'OSRIGHT';
+    KeyboardHandler.KEY_CONTEXTMENU = 'CONTEXTMENU';
+    KeyboardHandler.KEY_ENTER = 'ENTER';
+    KeyboardHandler.KEY_SPACE = 'SPACE';
+    KeyboardHandler.KEY_TAB = 'TAB';
+    KeyboardHandler.KEY_DELETE = 'DELETE';
+    KeyboardHandler.KEY_END = 'END';
+    KeyboardHandler.KEY_HOME = 'HOME';
+    KeyboardHandler.KEY_INSERT = 'INSERT';
+    KeyboardHandler.KEY_PAGEDOWN = 'PAGEDOWN';
+    KeyboardHandler.KEY_PAGEUP = 'PAGEUP';
+    KeyboardHandler.KEY_ESCAPE = 'ESCAPE';
+    KeyboardHandler.KEY_PRINTSCREEN = 'PRINTSCREEN';
+    KeyboardHandler.KEY_SCROLLLOCK = 'SCROLLLOCK';
+    KeyboardHandler.KEY_PAUSE = 'PAUSE';
 
     // TODO: Add numpad and function keys
 
     KeyboardHandler._keycodes_map = {
-        37: KeyboardHandler.KEYCODE_LEFT,
-        38: KeyboardHandler.KEYCODE_UP,
-        39: KeyboardHandler.KEYCODE_RIGHT,
-        40: KeyboardHandler.KEYCODE_DOWN,
-        48: KeyboardHandler.KEYCODE_0,
-        49: KeyboardHandler.KEYCODE_1,
-        50: KeyboardHandler.KEYCODE_2,
-        51: KeyboardHandler.KEYCODE_3,
-        52: KeyboardHandler.KEYCODE_4,
-        53: KeyboardHandler.KEYCODE_5,
-        54: KeyboardHandler.KEYCODE_6,
-        55: KeyboardHandler.KEYCODE_7,
-        56: KeyboardHandler.KEYCODE_8,
-        57: KeyboardHandler.KEYCODE_9,
-        65: KeyboardHandler.KEYCODE_A,
-        66: KeyboardHandler.KEYCODE_B,
-        67: KeyboardHandler.KEYCODE_C,
-        68: KeyboardHandler.KEYCODE_D,
-        69: KeyboardHandler.KEYCODE_E,
-        70: KeyboardHandler.KEYCODE_F,
-        71: KeyboardHandler.KEYCODE_G,
-        72: KeyboardHandler.KEYCODE_H,
-        73: KeyboardHandler.KEYCODE_I,
-        74: KeyboardHandler.KEYCODE_J,
-        75: KeyboardHandler.KEYCODE_K,
-        76: KeyboardHandler.KEYCODE_L,
-        77: KeyboardHandler.KEYCODE_M,
-        78: KeyboardHandler.KEYCODE_N,
-        79: KeyboardHandler.KEYCODE_O,
-        80: KeyboardHandler.KEYCODE_P,
-        81: KeyboardHandler.KEYCODE_Q,
-        82: KeyboardHandler.KEYCODE_R,
-        83: KeyboardHandler.KEYCODE_S,
-        84: KeyboardHandler.KEYCODE_T,
-        85: KeyboardHandler.KEYCODE_U,
-        86: KeyboardHandler.KEYCODE_V,
-        87: KeyboardHandler.KEYCODE_W,
-        88: KeyboardHandler.KEYCODE_X,
-        89: KeyboardHandler.KEYCODE_Y,
-        90: KeyboardHandler.KEYCODE_Z,
-        59: KeyboardHandler.KEYCODE_SEMICOLON,
-        61: KeyboardHandler.KEYCODE_EQUAL,
-        188: KeyboardHandler.KEYCODE_COMMA,
-        173: KeyboardHandler.KEYCODE_MINUS,
-        190: KeyboardHandler.KEYCODE_PERIOD,
-        191: KeyboardHandler.KEYCODE_SLASH,
-        192: KeyboardHandler.KEYCODE_BACKQUOTE,
-        219: KeyboardHandler.KEYCODE_BRACKETLEFT,
-        220: KeyboardHandler.KEYCODE_BACKSLASH,
-        221: KeyboardHandler.KEYCODE_BRACKETRIGHT,
-        222: KeyboardHandler.KEYCODE_QUOTE,
-        16: KeyboardHandler.KEYCODE_SHIFTLEFT,
-        16: KeyboardHandler.KEYCODE_SHIFTRIGHT,
-        17: KeyboardHandler.KEYCODE_CTRLLEFT,
-        17: KeyboardHandler.KEYCODE_CTRLRIGHT,
-        18: KeyboardHandler.KEYCODE_ALTLEFT,
-        18: KeyboardHandler.KEYCODE_ALTRIGHT,
-        20: KeyboardHandler.KEYCODE_CAPSLOCK,
-        91: KeyboardHandler.KEYCODE_OSLEFT,
-        92: KeyboardHandler.KEYCODE_OSRIGHT,
-        93: KeyboardHandler.KEYCODE_CONTEXTMENU,
-        13: KeyboardHandler.KEYCODE_ENTER,
-        32: KeyboardHandler.KEYCODE_SPACE,
-        9: KeyboardHandler.KEYCODE_TAB,
-        46: KeyboardHandler.KEYCODE_DELETE,
-        35: KeyboardHandler.KEYCODE_END,
-        36: KeyboardHandler.KEYCODE_HOME,
-        45: KeyboardHandler.KEYCODE_INSERT,
-        34: KeyboardHandler.KEYCODE_PAGEDOWN,
-        33: KeyboardHandler.KEYCODE_PAGEUP,
-        27: KeyboardHandler.KEYCODE_ESCAPE,
-        44: KeyboardHandler.KEYCODE_PRINTSCREEN,
-        145: KeyboardHandler.KEYCODE_SCROLLLOCK,
-        19: KeyboardHandler.KEYCODE_PAUSE
+        37: KeyboardHandler.KEY_LEFT,
+        38: KeyboardHandler.KEY_UP,
+        39: KeyboardHandler.KEY_RIGHT,
+        40: KeyboardHandler.KEY_DOWN,
+        48: KeyboardHandler.KEY_0,
+        49: KeyboardHandler.KEY_1,
+        50: KeyboardHandler.KEY_2,
+        51: KeyboardHandler.KEY_3,
+        52: KeyboardHandler.KEY_4,
+        53: KeyboardHandler.KEY_5,
+        54: KeyboardHandler.KEY_6,
+        55: KeyboardHandler.KEY_7,
+        56: KeyboardHandler.KEY_8,
+        57: KeyboardHandler.KEY_9,
+        65: KeyboardHandler.KEY_A,
+        66: KeyboardHandler.KEY_B,
+        67: KeyboardHandler.KEY_C,
+        68: KeyboardHandler.KEY_D,
+        69: KeyboardHandler.KEY_E,
+        70: KeyboardHandler.KEY_F,
+        71: KeyboardHandler.KEY_G,
+        72: KeyboardHandler.KEY_H,
+        73: KeyboardHandler.KEY_I,
+        74: KeyboardHandler.KEY_J,
+        75: KeyboardHandler.KEY_K,
+        76: KeyboardHandler.KEY_L,
+        77: KeyboardHandler.KEY_M,
+        78: KeyboardHandler.KEY_N,
+        79: KeyboardHandler.KEY_O,
+        80: KeyboardHandler.KEY_P,
+        81: KeyboardHandler.KEY_Q,
+        82: KeyboardHandler.KEY_R,
+        83: KeyboardHandler.KEY_S,
+        84: KeyboardHandler.KEY_T,
+        85: KeyboardHandler.KEY_U,
+        86: KeyboardHandler.KEY_V,
+        87: KeyboardHandler.KEY_W,
+        88: KeyboardHandler.KEY_X,
+        89: KeyboardHandler.KEY_Y,
+        90: KeyboardHandler.KEY_Z,
+        59: KeyboardHandler.KEY_SEMICOLON,
+        61: KeyboardHandler.KEY_EQUAL,
+        188: KeyboardHandler.KEY_COMMA,
+        173: KeyboardHandler.KEY_MINUS,
+        190: KeyboardHandler.KEY_PERIOD,
+        191: KeyboardHandler.KEY_SLASH,
+        192: KeyboardHandler.KEY_BACKQUOTE,
+        219: KeyboardHandler.KEY_BRACKETLEFT,
+        220: KeyboardHandler.KEY_BACKSLASH,
+        221: KeyboardHandler.KEY_BRACKETRIGHT,
+        222: KeyboardHandler.KEY_QUOTE,
+        16: KeyboardHandler.KEY_SHIFTLEFT,
+        16: KeyboardHandler.KEY_SHIFTRIGHT,
+        17: KeyboardHandler.KEY_CTRLLEFT,
+        17: KeyboardHandler.KEY_CTRLRIGHT,
+        18: KeyboardHandler.KEY_ALTLEFT,
+        18: KeyboardHandler.KEY_ALTRIGHT,
+        20: KeyboardHandler.KEY_CAPSLOCK,
+        91: KeyboardHandler.KEY_OSLEFT,
+        92: KeyboardHandler.KEY_OSRIGHT,
+        93: KeyboardHandler.KEY_CONTEXTMENU,
+        13: KeyboardHandler.KEY_ENTER,
+        32: KeyboardHandler.KEY_SPACE,
+        9: KeyboardHandler.KEY_TAB,
+        46: KeyboardHandler.KEY_DELETE,
+        35: KeyboardHandler.KEY_END,
+        36: KeyboardHandler.KEY_HOME,
+        45: KeyboardHandler.KEY_INSERT,
+        34: KeyboardHandler.KEY_PAGEDOWN,
+        33: KeyboardHandler.KEY_PAGEUP,
+        27: KeyboardHandler.KEY_ESCAPE,
+        44: KeyboardHandler.KEY_PRINTSCREEN,
+        145: KeyboardHandler.KEY_SCROLLLOCK,
+        19: KeyboardHandler.KEY_PAUSE
     };
 
 
